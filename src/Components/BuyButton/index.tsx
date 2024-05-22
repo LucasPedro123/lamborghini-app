a5ad40c330c90e3c8220f646fe1d5d22f92ffc3e
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-05-22 01:22:45 -0300

    feat: added components for Screens

---
+++ src/Components/BuyButton/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+
+import { AntDesign } from '@expo/vector-icons'
+import { styles } from './style'
+
+export default function BuyButton() {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button}>
+                <AntDesign
+                    name="shoppingcart"
+                    size={24}
+                    color='white'
+                    style={styles.icon}
+                />
+                <Text style={styles.text}> Buy This</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
---
+++ src/Components/BuyButton/style.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#01A6B3",
+        padding: 10, 
+        width: "80%",
+        alignItems: "center",
+        justifyContent: 'center',
+        alignSelf: 'center',
+        borderRadius: 8,
+    },
+    button: {
+        flexDirection: 'row',
+        
+    },
+    icon: {
+        marginRight: 20
+    },
+    text: {
+        color: "#fff",
+        fontWeight: 'bold',
+        fontStyle: "italic",
+    }
+})
